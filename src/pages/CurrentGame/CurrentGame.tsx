dc1c842cc2e10c3b942481770e3807700cc05ede
Egor Rudenko <egor.rudenko.msc@gmail.com>
2025-04-08 23:43:08 +0300

    Redirect logic and winning conditions last. Timer is finished

--- src/pages/CurrentGame/CurrentGame.tsx
+++ src/pages/CurrentGame/CurrentGame.tsx
@@ -11,7 +11,7 @@ import { Board } from "@/widgets/Board";
 import parseFEN from "./helpers/parseFEN";
 import styles from "./CurrentGame.module.css";
 import { useEffect, useLayoutEffect, useMemo, useState } from "react";
-import { resetBoardSlice } from "@/app";
+import { changeTurn, resetBoardSlice } from "@/app";
 
 const CurrentGame = () => {
   const { id } = useParams<{ id: string }>();
@@ -46,61 +46,82 @@ const CurrentGame = () => {
 
   const { turn } = useSelectorTs(selector);
 
-  useEffect(() => {
-    let intervalId: ReturnType<typeof setInterval>;
-    let wasBonusUsed: boolean = false;
-    if (turn === null) {
-    } else {
-      intervalId = setInterval(() => {
-        const games = JSON.parse(
-          localStorage.getItem("games")
-        ) as unknown as gamesI;
-        const currentGame = games[id];
-        const timer = currentGame.timerInfo.timer;
-        let seconds = parseInt(timer[turn][1]);
-        let minutes = parseInt(timer[turn][0]);
-        if (timer[turn][0] === currentGame.timerInfo.startMinutes) {
-          wasBonusUsed = true;
+  //IIFE использовано
+  useEffect(
+    (() => {
+      let isMetWithStartTimer = false;
+      return () => {
+        let intervalId: ReturnType<typeof setInterval>;
+        let wasBonusUsed: boolean = false;
+        if (turn === null) {
         } else {
-          if (!wasBonusUsed) {
-            seconds += Number(currentGame.timerInfo.bonus);
-            wasBonusUsed = true;
-            if (seconds >= 60) {
-              minutes += 1;
-              seconds = seconds - 60;
+          intervalId = setInterval(() => {
+            const games = JSON.parse(
+              localStorage.getItem("games")
+            ) as unknown as gamesI;
+            const currentGame = games[id];
+            const timer = currentGame.timerInfo.timer;
+            let seconds = parseInt(timer[turn][1]);
+            let minutes = parseInt(timer[turn][0]);
+            if (seconds === 0 && minutes === 0) {
+              dispatch(changeTurn(null));
+              clearInterval(intervalId);
+              return;
             }
-          }
+            if (
+              timer[turn][0] === currentGame.timerInfo.startMinutes &&
+              !isMetWithStartTimer
+            ) {
+              wasBonusUsed = true;
+              isMetWithStartTimer = true;
+            } else {
+              if (!wasBonusUsed) {
+                seconds += Number(currentGame.timerInfo.bonus);
+                wasBonusUsed = true;
+                if (seconds >= 60) {
+                  minutes += 1;
+                  seconds = seconds - 60;
+                }
+              }
+            }
+            if (seconds === 0) {
+              minutes -= 1;
+              seconds = 59;
+            } else {
+              seconds -= 1;
+            }
+            const newTime = [
+              minutes.toString(),
+              seconds.toString().length === 1
+                ? "0" + seconds.toString()
+                : seconds.toString(),
+            ];
+            localStorage.setItem(
+              "games",
+              JSON.stringify({
+                ...games,
+                [id]: {
+                  ...currentGame,
+                  timerInfo: {
+                    ...currentGame.timerInfo,
+                    timer: { ...timer, [turn]: newTime },
+                  },
+                },
+              })
+            );
+            if (newTime[1].length === 1) {
+              newTime[1] = "0" + newTime[1];
+            }
+            setGameTimer((prev) => ({ ...prev, [turn]: newTime.join(":") }));
+          }, 1000);
         }
-        if (seconds === 0) {
-          minutes -= 1;
-          seconds = 59;
-        } else {
-          seconds -= 1;
-        }
-        const newTime = [minutes.toString(), seconds.toString()];
-        localStorage.setItem(
-          "games",
-          JSON.stringify({
-            ...games,
-            [id]: {
-              ...currentGame,
-              timerInfo: {
-                ...currentGame.timerInfo,
-                timer: { ...timer, [turn]: newTime },
-              },
-            },
-          })
-        );
-        if (newTime[1].length === 1) {
-          newTime[1] = "0" + newTime[1];
-        }
-        setGameTimer((prev) => ({ ...prev, [turn]: newTime.join(":") }));
-      }, 1000);
-    }
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [turn]);
+        return () => {
+          clearInterval(intervalId);
+        };
+      };
+    })(),
+    [turn]
+  );
 
   return (
     <div className={styles.container}>
@@ -110,6 +131,7 @@ const CurrentGame = () => {
       <div className={styles.info}>
         <div style={{ color: "white" }}>{gameTimer.white}</div>
         <div style={{ color: "black" }}>{gameTimer.black}</div>
+        <div>{turn}</div>
       </div>
     </div>
   );
